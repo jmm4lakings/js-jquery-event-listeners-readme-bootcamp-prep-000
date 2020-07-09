e829cae9ebff3648b196a51967a4ccfab20d6f7e
Justin Mayer <justin_mayer2003@yahoo.com>
2020-07-09 16:24:15 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,25 @@
 //define functions here
 
+function getIt() {
+  $('p').on("click", function () {
+    alert("Hey!");
+  })
+   }
+   
+   function frameIt() {
+     $('img').on("load", function() {
+       $('img').addClass("tasty");
+     })
+   }
+
+function pressIt() {
+  $('#typing').on("keydown", function(press) {
+    if (press.which === 71) {
+      alert("You have entered g.");
+    }
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
